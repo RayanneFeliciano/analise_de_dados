b64a90da9e3eb9d6cd572c5e906ec15bdbcf1df0
Rayanne Feliciano <ray_anne_af@hotmail.com>
2023-04-15 15:42:35 -0300

    Atividade para criação de objeto e utilização dele numa função

---
+++ objeto_e_funcao.R
@@ -0,0 +1,4 @@
+MeuObjeto <- runif(7, 10, 100) #Criação do objeto, chamando-o de "Meu Objeto"
+MeuObjeto  #Chamando o "Meu Objeto" dado que é composto por uma função
+sort(MeuObjeto, decreasing = TRUE) "Aplicação de uma função ao 'Meu Objeto'"
+str(MeuObjeto) "Verificação da complexidade do objeto criado"
