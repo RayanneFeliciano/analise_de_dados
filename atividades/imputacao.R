2d3b363708dfcfd9e13221bfb89416b2046c0ace
Rayanne Feliciano <129545969+RayanneFeliciano@users.noreply.github.com>
2023-05-26 08:52:47 -0300

    Update imputacao.R

--- atividades/imputacao.R
+++ atividades/imputacao.R
@@ -6,21 +6,21 @@ data(food, package = 'VIM')
 status(food)
 view(food)
 
-BaseOriginal <- copy(food)
+baseOriginal <- copy(food)
 
 # tendência central
-(BaseOriginal$Sweetener <- impute(BaseOriginal$Sweetener, fun = mean))
+(baseOriginal$Sweetener <- impute(baseOriginal$Sweetener, fun = mean))
 
 # teste se o valor foi imputado
-is.imputed(BaseOriginal$Sweetener)
+is.imputed(baseOriginal$Sweetener)
 
 #tabela de imputação por sim/não
-table(is.imputed(BaseOriginal$Sweetener))
+table(is.imputed(baseOriginal$Sweetener))
 
 ## Hot deck
 
 # Volta para a base original que possui NA
-BaseOriginal <- copy(food)
+baseOriginal <- copy(food)
 
 # imputação por instâncias/semelhanças
-(BaseImputada <- kNN(BaseOriginal))
+(baseImputada <- kNN(baseOriginal))
