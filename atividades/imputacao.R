2d9ba01a5dd35f6cb8eeb6f33e882e7764d66982
Rayanne Feliciano <ray_anne_af@hotmail.com>
2023-05-26 08:40:40 -0300

    imputação numérica e hot deck

---
+++ atividades/imputacao.R
@@ -0,0 +1,27 @@
+# imputação de valores em outliers ou missing
+pacman::p_load(data.table, Hmisc, VIM) # carrega pacotes
+
+## imputação numérica
+data(food, package = 'VIM')
+status(food)
+view(food)
+
+BaseOriginal <- copy(food)
+
+# tendência central
+(BaseOriginal$Sweetener <- impute(BaseOriginal$Sweetener, fun = mean))
+(BaseOriginal$Sweetener <- impute(BaseOriginal$Sweetener, fun = median))
+
+# teste se o valor foi imputado
+is.imputed(BaseOriginal$Sweetener)
+
+#tabela de imputação por sim/não
+table(is.imputed(BaseOriginal$Sweetener))
+
+## Hot deck
+
+# Volta para a base original que possui NA
+BaseOriginal <- copy(food)
+
+# imputação por instâncias/semelhanças
+(BaseImputada <- kNN(BaseOriginal))
