72da2b335f0c9aabad42451aeb8953e968d95053
Rayanne Feliciano <129545969+RayanneFeliciano@users.noreply.github.com>
2023-06-02 18:07:24 -0300

    teste_hipótese.R

--- atividades/teste_hipótese.R
+++ atividades/teste_hipótese.R
@@ -1,7 +1,7 @@
 pacman::p_load(data.table)
 
 # carrega a base
-load("C:/Users/ray_a/Downloads/Latinobarometro_2020_Esp_Rdata_v1_0.rdata")
+load("C:/Users/ray_a/Documents/analise_de_dados/bases_originais/Latinobarometro_2020_Esp_Rdata_v1_0.rdata")
 
 # relação qui-quadrado para verificar a associação entre situação financeira e apoio à democracia
 chisq.test(Latinobarometro_2020_Esp$p8st.a, Latinobarometro_2020_Esp$p10stgbs)
